3c64e0901bbf37b8f90ebf26f38056b67fb472b1
Harsha <k.sri.harsha@accenture.com>
2022-04-07 16:34:13 +0530

    mage changes

--- main.tf
+++ main.tf
@@ -75,5 +75,6 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     machine_type = "n1-standard-1"
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = "terraformsa@peerless-rock-346305.iam.gserviceaccount.com"
   }
 }
