ec4c94dd7682f9ddb6a445d12e3f4aa561eb124c
Harsha <k.sri.harsha@accenture.com>
2022-04-07 15:59:41 +0530

    added GKE

--- main.tf
+++ main.tf
@@ -52,3 +52,28 @@ resource "google_compute_instance" "default" {
 //     scopes = ["cloud-platform"]
 //   }
 }
+
+resource "google_container_cluster" "primary" {
+  name     = "harsha-cluster"
+  location = "asia-south2"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "asia-south2"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+  }
+}
