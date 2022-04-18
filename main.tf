600fcc541ae63a38afb1ad0925eeab17668fbfc5
sriharshakothuri <78167630+sriharshakothuri@users.noreply.github.com>
2022-04-18 16:45:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,51 +7,51 @@ terraform {
   }
 }
 
-resource "google_compute_instance" "default" {
-  name         = "jenkinsserver"
-  machine_type = "n1-standard-2"
-  zone         = "asia-south2-c"
+# resource "google_compute_instance" "default" {
+#   name         = "jenkinsserver"
+#   machine_type = "n1-standard-2"
+#   zone         = "asia-south2-c"
 
-  tags = ["foo", "bar"]
+#   tags = ["foo", "bar"]
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-9"
+#     }
+#   }
 
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
+#   // Local SSD disk
+#   scratch_disk {
+#     interface = "SCSI"
+#   }
 
-  network_interface {
-    network = "default"
+#   network_interface {
+#     network = "default"
 
-    access_config {
-      // Ephemeral public IP
-    }
-  }
+#     access_config {
+#       // Ephemeral public IP
+#     }
+#   }
 
-//   metadata = {
-//     foo = "bar"
-//   }
+# //   metadata = {
+# //     foo = "bar"
+# //   }
 
-  metadata_startup_script = <<SCRIPT
-  #!/bin/bash
-  curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-  echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-  sudo apt-get update
-  sudo apt-get install openjdk-11-jre -y
-  sudo apt-get install jenkins -y
-  SCRIPT
+#   metadata_startup_script = <<SCRIPT
+#   #!/bin/bash
+#   curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+#   echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+#   sudo apt-get update
+#   sudo apt-get install openjdk-11-jre -y
+#   sudo apt-get install jenkins -y
+#   SCRIPT
 
-//   service_account {
-//     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-//     email  = google_service_account.default.email
-//     scopes = ["cloud-platform"]
-//   }
-}
+# //   service_account {
+# //     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+# //     email  = google_service_account.default.email
+# //     scopes = ["cloud-platform"]
+# //   }
+# }
 
 resource "google_container_cluster" "primary" {
   name     = "harsha-cluster"
