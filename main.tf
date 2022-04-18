8371f6c692e1e73487c28090fe09110cb1e63118
sriharshakothuri <78167630+sriharshakothuri@users.noreply.github.com>
2022-04-18 14:38:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "google_compute_instance" "default" {
 
 resource "google_container_cluster" "primary" {
   name     = "harsha-cluster"
-  location = "asia-south2"
+  location = "asia-south2-c"
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
