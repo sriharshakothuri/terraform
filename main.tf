666441d48bcc87d810588b885059aab56b416e9c
sriharshakothuri <78167630+sriharshakothuri@users.noreply.github.com>
2022-04-20 14:31:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -76,5 +76,8 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = "terraformsa2@harsha-346511.iam.gserviceaccount.com"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.full_control"
+    ]
   }
 }
